5622a7951b7ce192a784153846870af6de2853fe
Liu Yu-Shun <raisoliu@gmail.com>
2023-03-16 12:35:08 +0800

    Update plot.py

--- btool/plot.py
+++ btool/plot.py
@@ -3,9 +3,9 @@ from glob import glob
 import imageio.v2 as imageio
 from tqdm import tqdm
 
-def imgs2gif(anim_file, imgs):
+def imgs2gif(ani, imgs):
   
-  with imageio.get_writer(anim_file, mode='I') as writer:
+  with imageio.get_writer(ani, mode='I') as writer:
       li = sorted(glob(join(imgs, '*.png')))
       print(f"Find {len(li)} picture(s)")
       for it in tqdm(li):
