be83ea974e1321a9d9bb8c0d22cbaf7b813689b6
Liu Yu-Shun <raisoliu@gmail.com>
2023-03-16 12:34:40 +0800

    Update plot.py

--- btool/plot.py
+++ btool/plot.py
@@ -1,10 +1,14 @@
-import os
-import glob
+from import os.path import join
+from glob import glob
 import imageio.v2 as imageio
+from tqdm import tqdm
 
-def imgs2gif(anim_file, img_folder):
+def imgs2gif(anim_file, imgs):
+  
   with imageio.get_writer(anim_file, mode='I') as writer:
-      filenames = sorted(glob.glob(os.path.join(img_folder, '*.png')))
-      for filename in filenames:
-          image = imageio.imread(filename)
+      li = sorted(glob(join(imgs, '*.png')))
+      print(f"Find {len(li)} picture(s)")
+      for it in tqdm(li):
+          image = imageio.imread(it)
           writer.append_data(image)
+  
